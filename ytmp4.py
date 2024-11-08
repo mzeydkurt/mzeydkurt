ab52ad2ee444d8e9d1d0377d9368c9aa618dd801
Muhammed Zeyd KURT <zeydhfz10@gmail.com>
2024-11-08 23:25:11 +0300

    Youtube Dowloand MP4 Videos

---
+++ ytmp4.py
@@ -0,0 +1,16 @@
+import yt_dlp
+user_input = input("İndirmek İstediğiniz Videonun Linkini Giriniz : ")
+
+try:
+    # İndirme ayarlarını tanımla
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': '%(title)s.%(ext)s',
+    }
+
+    # Video indirme
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([user_input])
+    print("Video başarıyla indirildi!")
+except Exception as e:
+    print("Hata Oluştu ", e)
